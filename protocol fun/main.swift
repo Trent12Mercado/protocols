b0b70228d6cf4dc7e2619e1e532e2654d8072d1d
Trent Mercado <tmercado@zagmail.gonzaga.edu>
2020-09-20 17:03:04 -0700

    Initial Commit

---
+++ protocol fun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  protocol fun
+//
+//  Created by Mercado, Trenton Laurent on 9/20/20.
+//  Copyright © 2020 Mercado, Trenton Laurent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
